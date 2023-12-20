bd10c653e9d4318c585f9c6ffd6672004adf09ea
Andrea Calderon <andreacalderon@MacBook-Pro-2.local>
2023-12-19 19:07:01 -0500

    added ReviewController and post, created Favorite controller and book controller

---
+++ FullStackAuth_WebAPI/Controllers/ReviewsController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FullStackAuth_WebAPI.Data;
+using FullStackAuth_WebAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FullStackAuth_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReviewsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/Reviews
+        [HttpPost, Authorize]
+        public IActionResult Post([FromBody] Review data)
+        {
+        }
+
+      
+    }
+}
